2dae76b6b53cce2fbbd5825d507f08b74090d1cd
AznC2W <chichunwan3@gmail.com>
2015-01-07 13:00:06 +0100

    change passenger number from 100 to 90

--- src/Traffic.py
+++ src/Traffic.py
@@ -59,7 +59,7 @@ class Traffic(object):
             traffic[i] = Airplane(i, self.cities[i], self.cities[i], self.COLOR[i])
 
             city1 = traffic[i].getCurrentCity()
-            passengers1 = 100
+            passengers1 = 90
             city2 = self.cities[5]
             passengers2 = 100
             city3 = self.cities[14]
